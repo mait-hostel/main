2b66e10892da8c0a08187017c5cb4c617877719d
Kapil218 <103837696+Kapil218@users.noreply.github.com>
2022-09-06 18:57:33 +0530

    all is set

--- scripts/landing-page-image-gallery.js
+++ scripts/landing-page-image-gallery.js
@@ -1,28 +1,31 @@
-let slideIndex = 1;
+var slideIndex = 1;
+var timer = null;
 showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
+  clearTimeout(timer);
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
+  clearTimeout(timer);
   showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n==undefined){n = ++slideIndex}
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+      slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+      dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
-}
+  timer = setTimeout(showSlides, 5000);
+} 
